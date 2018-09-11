872e0e333dea9978d14865cb163c649055d1eed7
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-09-11 12:35:56 -0500

    init.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # train-scheduler
+
+[Train Scheduler](
+https://raywon123.github.io/train-scheduler/ )
+
+[My Portfolio Page](
+https://raywon123.github.io/portfolio.html )
+
+#### How It Works:
+*
+
+#### How It Is Built:
+*
+
+#### How To Test:
+*
+
+#### What Needs To Be Done:
+*
---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    // This function remove element from an array
+    function removeElement(array, element) {
+        let index = array.indexOf(element);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+
+    // This function counts element occurances in an array
+    function countElement(array, element) {
+        let counts = {};
+        for (var i = 0; i < array.length; i++) {
+            if (!counts.hasOwnProperty(array[i])) {
+                counts[array[i]] = 1;
+            }
+            else {
+                counts[array[i]]++;
+            }
+        }
+        return counts[element];
+    }
+
+    // This function finds index of duplicate elements in an array
+    function findDuplicateElement(array, element) {
+        let duplicates = {};
+        for (var i = 0; i < array.length; i++) {
+            if (duplicates.hasOwnProperty(array[i])) {
+                duplicates[array[i]].push(i);
+            }
+            else if (array.lastIndexOf(array[i]) !== i) {
+                duplicates[array[i]] = [i];
+            }
+        }
+        return duplicates[element];
+    }
+
+    // -- main program
+
+    let firstNumber = 0;
+
+    $(".number").on("click", function () {
+        console.log($(this).val());
+        firstNumber += $(this).val();
+        $("#first-number").text(firstNumber);
+        console.log(firstNumber);
+        let result = firstNumber + 2;  // '2 '+ '2' = 22  string addition
+        console.log("result=" + result);
+    });
+
+});
